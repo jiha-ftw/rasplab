7433e6a8020b7152cb1f546d6c42e17fe8fce979
jim <jim@ftwconsulting.se>
2023-08-10 11:43:02 +0200

    rm dup code

--- RaspberryPI/SendData.py
+++ RaspberryPI/SendData.py
@@ -8,16 +8,6 @@ import paho.mqtt.publish as publish
 instance_id = os.getenv("RASPLAB_INSTANCE_IDENTIFIER")
 humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4)
 
-publish.single(
-	"mqtt/collect-temperature",
-	f"{{ \"Source\": {instance_id}, \"Temperature\": {temperature}, \"Humidity\": {humidity} }}",
-	hostname = os.getenv("RASPLAB_MOSQUITTO_SERVER_IP"),
-	auth = { 
-		"username": "mosquitto_user",
-		"password": os.getenv("RASPLAB_MOSQUITTO_USER_PASSWORD") 
-	}
-)
-
 GREEN_PIN = 19
 RED_PIN = 26
 SIGNAL_PIN = GREEN_PIN
